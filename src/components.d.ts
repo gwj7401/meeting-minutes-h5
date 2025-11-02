d6b8257d9cd22071572e61d1ce54af635ceede10
gwj7401 <gwjyyl@126.com>
2025-11-02 14:25:40 +0800

    feat: 添加 Android 项目配置，支持打包 APK

--- src/components.d.ts
+++ src/components.d.ts
@@ -15,7 +15,6 @@ declare module 'vue' {
     VanButton: typeof import('vant/es')['Button']
     VanCell: typeof import('vant/es')['Cell']
     VanCellGroup: typeof import('vant/es')['CellGroup']
-    VanDialog: typeof import('vant/es')['Dialog']
     VanEmpty: typeof import('vant/es')['Empty']
     VanField: typeof import('vant/es')['Field']
     VanFloatingBubble: typeof import('vant/es')['FloatingBubble']
